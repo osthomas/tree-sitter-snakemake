ead78ba733f001066312b112aa036812a2941a2b
Oliver Thomas <thomaso@ie-freiburg.mpg.de>
2023-01-07 15:58:42 +0100

    Add Python parser and scanner dependency

--- grammar.js
+++ grammar.js
@@ -1,6 +1,9 @@
-module.exports = grammar({
+const PYTHON = require("tree-sitter-python/grammar")
+
+module.exports = grammar(PYTHON, {
     name: "snakemake",
 
     rules: {
+        _test: $ => "abc"
     }
 });
