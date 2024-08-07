bb9b73b9c443ca9cf56b739bf92831d65d075037
Oliver Thomas <thomaso@ie-freiburg.mpg.de>
2024-08-07 10:29:07 +0200

    feat: minor query updates, format

--- queries/snakemake/highlights.scm
+++ queries/snakemake/highlights.scm
@@ -13,10 +13,9 @@
     name: _ @keyword))
 
 ; Subordinate directives (eg. input, output)
-((_)
-  body: (_
-    (directive
-      name: _ @label)))
+body: (_
+  (directive
+    name: _ @label))
 
 ; rule/module/checkpoint names
 (rule_definition
@@ -30,12 +29,14 @@
 
 ; Rule imports
 (rule_import
-  "use" @keyword.import
-  "rule" @keyword.import
-  "from" @keyword.import
-  "exclude"? @keyword.import
-  "as"? @keyword.import
-  "with"? @keyword.import)
+  [
+    "use"
+    "rule"
+    "from"
+    "exclude"
+    "as"
+    "with"
+  ] @keyword.import)
 
 ; Rule inheritance
 (rule_inheritance
@@ -44,8 +45,11 @@
   "with" @keyword)
 
 ; Wildcard names
-(wildcard (identifier) @variable)
-(wildcard (flag) @variable.parameter.builtin)
+(wildcard
+  (identifier) @variable)
+
+(wildcard
+  (flag) @variable.parameter.builtin)
 
 ; builtin variables
 ((identifier) @variable.builtin
